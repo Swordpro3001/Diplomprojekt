fb14fadde4c9257cb8835a3530e9b8e963c033fb
Franz Puerto <puertofranz@gmail.com>
2025-10-02 09:14:30 +0200

    [Feat] About Us section and Footer

--- Project_website/src/app/app.ts
+++ Project_website/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Header} from './header/header';
+import { Footer } from "./footer/footer";
+import { Team } from "./team/team";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer, Team],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
