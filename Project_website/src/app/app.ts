d5f544506dd6e1913181c6af472494b0e693af51
Franz Puerto <puertofranz@gmail.com>
2025-10-06 14:46:08 +0200

    [Feat]: created-about section

--- Project_website/src/app/app.ts
+++ Project_website/src/app/app.ts
@@ -1,11 +1,16 @@
-import { Component, signal } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Header} from './header/header';
+import { Footer } from "./footer/footer";
+import { Team } from "./team/team";
+import { SwiperContainer } from 'swiper/element';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer, Team],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class App {
   protected readonly title = signal('Project_website');
